b07db12c84b0841f89524799e96eadb4338ee3ce
Ata Çetin <ihavenick@gmail.com>
2021-06-06 14:41:03 +0300

    zaman

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -18,8 +18,12 @@ public class GameManager : MonoBehaviour
     public GameObject YEMEKTEXT;
 
     private GameObject kaybettin;
+    
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
 
     public int bolumNum;
+    public Text timeText;
     
     void Start()
     {
@@ -39,6 +43,8 @@ public class GameManager : MonoBehaviour
             case 2:
                 PARATEXT.GetComponent<Text>().text = AltinSayisi.ToString();
                 YEMEKTEXT.GetComponent<Text>().text = YemekSayisi.ToString();
+                timerIsRunning = true;
+                timeRemaining = 60;
                 break;
             default:
                 bolumNum = 0;
@@ -102,6 +108,33 @@ public class GameManager : MonoBehaviour
             VillageSpawner.GetComponent<VillagerSpawner>().SpawnVillager = false;
             kaybettin.SetActive(true);
             kaybettin.GetComponent<Text>().text += "\n Köylüler sinirlenip seni linç etti";
+        }
+    }
+    
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+    
+    
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+            DisplayTime(timeRemaining);
         }
     }
     
