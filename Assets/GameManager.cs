eb9b5ff7d48dae1c23926d42cba49176eed05cc2
Ata Çetin <ihavenick@gmail.com>
2021-06-06 14:16:00 +0300

    para spawn ve para

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -18,18 +18,31 @@ public class GameManager : MonoBehaviour
     public GameObject YEMEKTEXT;
 
     private GameObject kaybettin;
+
+    public int bolumNum;
     
     void Start()
     {
-        kaybettin = GameObject.Find("Kaybettin");
-        VillageSpawner = GameObject.Find("VillagerSpawner");
-        kaybettin.SetActive(false);
-        KralBar.GetComponent<Slider>().value = KralMetre;
+        switch (bolumNum)
+        {
+            case 1 :
+                kaybettin = GameObject.Find("Kaybettin");
+                VillageSpawner = GameObject.Find("VillagerSpawner");
+                kaybettin.SetActive(false);
+                KralBar.GetComponent<Slider>().value = KralMetre;
         
-        HalkBar.GetComponent<Slider>().value = KoyluMetre;
+                HalkBar.GetComponent<Slider>().value = KoyluMetre;
 
-        PARATEXT.GetComponent<Text>().text = AltinSayisi.ToString();
-        YEMEKTEXT.GetComponent<Text>().text = YemekSayisi.ToString();
+                PARATEXT.GetComponent<Text>().text = AltinSayisi.ToString();
+                YEMEKTEXT.GetComponent<Text>().text = YemekSayisi.ToString();
+                break;
+            case 2:
+                
+                break;
+            default:
+                bolumNum = 0;
+                break;
+        }
     }
 
     public bool Kabul(int altin, int yemek)
