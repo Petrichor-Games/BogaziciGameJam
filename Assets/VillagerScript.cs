20d5a38d7a8fa39510b6c8e71302a816bb4bb299
Ata Çetin <ihavenick@gmail.com>
2021-06-06 11:41:04 +0300

    villager soru sorma

--- Assets/VillagerScript.cs
+++ Assets/VillagerScript.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class VillagerScript : MonoBehaviour
 {
+    public Villager VillagerClass;
+    public GameObject KonusmaBalonu;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("SoruSor", 2);
     }
 
+    void SoruSor()
+    {
+        var soru = Instantiate(KonusmaBalonu, transform);
+        soru.GetComponentInChildren<TextMesh>().text = VillagerClass.ask_food.ToString() + " Yemek istiyorum";
+    }
+    
+    
+    
+    
     // Update is called once per frame
     void Update()
     {
